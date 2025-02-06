cc7c117845fc949d7931a2a045aa79bbe4ed5e8c
korawit-cric <kor@criclabs.co>
2025-02-06 17:36:09 +0700

    monex-root-feat: remove backend apps for now

--- apps/backend/src/repositories/__test__/mock.ts
+++
@@ -1,5 +0,0 @@
-// sum.js
-export function sum(a, b) {
-   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-   return a + b
- }
--- apps/backend/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Express } from "express";
-import userRoute from "./user.route";
-
-const initializeRouter = (app: Express) => {
-  app.use(userRoute);
-};
-
-export default initializeRouter;
