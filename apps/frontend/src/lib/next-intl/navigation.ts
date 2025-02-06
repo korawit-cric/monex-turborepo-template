694849e2bbc24e3d8a69f00fccfa6d01caa92845
korawit-cric <kor@criclabs.co>
2025-02-07 01:25:39 +0700

    feat(monex-root): add intl navigation

--- apps/frontend/src/lib/next-intl/navigation.ts
+++ apps/frontend/src/lib/next-intl/navigation.ts
@@ -1,3 +1,4 @@
+import { createNavigation } from 'next-intl/navigation';
 import { defineRouting } from 'next-intl/routing';
 
 export const locales = ['en', 'th'] as const;
@@ -5,5 +6,8 @@ export const localePrefix = 'always';
 export const defaultLocale = 'th';
 export const routing =
   defineRouting({ locales, defaultLocale, localePrefix });
+
+export const { Link, redirect, usePathname, useRouter, getPathname } =
+  createNavigation(routing);
 
 export type Locale = (typeof locales)[number];
